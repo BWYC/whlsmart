cf236097b44217f21d45caa1ff805f390d92dfcf
BWYC <129849126+BWYC@users.noreply.github.com>
2023-09-07 08:11:45 +0000

    1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,26 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Navbar } from "../components/Navbar/Navbar";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { CoreBlockchain } from "@thirdweb-dev/chains";
 import NextNProgress from "nextjs-progressbar";
 import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    
+const sdk = new ThirdwebSDK(CoreBlockchain, {
+  clientId: "65a85b91315ca838d7a8472fb0e64f92",
+})
+
+
+
+export default function MyApp({
+  Component: Component,
+  pageProps: { ...pageProps },
+  NextNProgress: NextNProgress
+}) {
+  return(
+    <>
     <ThirdwebProvider
-      clientId="65a85b91315ca838d7a8472fb0e64f92"
       activeChain={NETWORK}
     >
 
@@ -21,13 +32,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         showOnShallow={true}
       />
 
-
+      {/* Render the navigation menu above each component */}
       <Navbar />
 
       
       <Component {...pageProps} />
     </ThirdwebProvider>
+    </>
   );
 }
 
-export default MyApp;
+

cbbf8e7790d6987e3f9ffa65a1b284bce0329b0e
BWYC <129849126+BWYC@users.noreply.github.com>
2023-09-07 08:36:28 +0000

    1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,31 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Navbar } from "../components/Navbar/Navbar";
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { CoreBlockchain } from "@thirdweb-dev/chains";
 import NextNProgress from "nextjs-progressbar";
 import { NETWORK } from "../const/contractAddresses";
 import "../styles/globals.css";
 
-const sdk = new ThirdwebSDK(CoreBlockchain, {
-  clientId: "65a85b91315ca838d7a8472fb0e64f92",
-})
-
-
-
-export default function MyApp({
-  Component: Component,
-  pageProps: { ...pageProps },
-}) {
-  return(
-    <>
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
     <ThirdwebProvider
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={NETWORK}
     >
-
+      {/* Progress bar when navigating between pages */}
       <NextNProgress
-        color="orange"
+        color="var(--color-tertiary)"
         startPosition={0.3}
         stopDelayMs={200}
-        height={4}
+        height={3}
         showOnShallow={true}
       />
 
       {/* Render the navigation menu above each component */}
       <Navbar />
-
-      
+      {/* Render the actual component (page) */}
       <Component {...pageProps} />
     </ThirdwebProvider>
-    </>
   );
 }
 
-
+export default MyApp;
